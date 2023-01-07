878eb5e61b8d0fbd0506fb5d12e10fa20b42fa70
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2023-01-07 12:23:20 +0100

    Updates

---
+++ precache-manifest.703b405ecf8e3dddbdd6cfa149f36ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70c86b434314478b3585",
+    "url": "/static/css/main.95ffe06c.chunk.css"
+  },
+  {
+    "revision": "70c86b434314478b3585",
+    "url": "/static/js/main.70c86b43.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7f15376940deeb7f36313cc4de9c9c5e",
+    "url": "/index.html"
+  }
+];
